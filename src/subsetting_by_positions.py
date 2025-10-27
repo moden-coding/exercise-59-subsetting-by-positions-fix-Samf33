9c65a16489ad6a7215bd35fdd2711113152c9f4f
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-10-27 08:45:23 -0400

    done

--- src/subsetting_by_positions.py
+++ src/subsetting_by_positions.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 def subsetting_by_positions():
     df = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t', index_col=0)
-    pass
+    return df.iloc[0:10, 1:3]
 
 def main():
-    subsetting_by_positions()
+    print(subsetting_by_positions())
 
 if __name__ == "__main__":
     main()
